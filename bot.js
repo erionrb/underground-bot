ffab8e4dfd2ac9f5f1f83a0f4e50dc6492c7782b
erionrb <erionrb@gmail.com>
2020-10-16 08:31:17 -0300

    fixing heroku

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+//const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
 const botTask = require("./com/bot/common/botTask")();
 
